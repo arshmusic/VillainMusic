4849256a7f18828de623e06b9f1ed6a6e3fb4f6c
arshmusic <bchat837@gmail.com>
2024-12-04 08:40:10 +0530

    Update couples.py

--- SONALI/plugins/tools/couples.py
+++ SONALI/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from SONALI.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="ꪜ 𝛊 ɭ ɭ ᧘ 𝛊 𝛈",
-            url=f"https://t.me/iamakki001",
+            text="𝘼𝙍𝙎𝙃 💀🍷",
+            url=f"https://t.me/Heartle5s9",
         ),
     ],
 ]
